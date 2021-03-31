21f520c8aae0a21baf56bb4ce4b718d8c943e590
Adia Boyd <72044418+codeyab22@users.noreply.github.com>
2021-03-30 23:36:47 -0400

    json.js
    I am adding the names of the employees as well as the number of the ids.

---
+++ src/Data/json.js
@@ -0,0 +1,28 @@
+const persons = [
+    {
+        userId: 1,
+        image: "",
+        name: { first: "Adia", last: "Boyd" },
+        phone: "9733504655",
+        email: "codeyab704@gmail.com",
+        dob: "12-24-1992"
+    },
+    {
+        userId: 2,
+        image: "",
+        name: { first: "Daja", last: "West" },
+        phone: "(512) 123-8910",
+        email: "dajas304gmail.com",
+        dob: ""03-23-1994"
+    },
+    {
+        userId: 3,
+        image: "",
+        name: { first: "FC", last: "Murray" },
+        phone: "2354430",
+        email: "",
+        dob: "07-23-1993"
+    }
+]
+
+export default persons;
